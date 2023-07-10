e1cde084b171e5d235a60bbb585d7b41d73813f5
0xfsk <furkankulaksiz68@gmail.com>
2023-07-11 00:28:15 +0300

    Initial commit

---
+++ src/main/java/com/sekomproject/sekom/SekomApplication.java
@@ -0,0 +1,13 @@
+package com.sekomproject.sekom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SekomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SekomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sekomproject/sekom/SekomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sekomproject.sekom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SekomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
